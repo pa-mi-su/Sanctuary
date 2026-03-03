4ca252425f6255f0f3914aa21b3d5630d8cf4ae4
pa-mi-su <pamisu@protonmail.com>
2026-03-03 12:57:46 -0500

    Initial Commit

---
+++ Sanctuary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sanctuary
+//
+//  Created by PMS on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sanctuary/SanctuaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanctuaryApp.swift
+//  Sanctuary
+//
+//  Created by PMS on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SanctuaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
